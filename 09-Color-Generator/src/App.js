d9f3e73dd20c551a2ccf228dafcc16aac4dc2b57
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-17 17:55:41 +0900

    09-1 Structure

--- 09-Color-Generator/src/App.js
+++ 09-Color-Generator/src/App.js
@@ -1,10 +1,40 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const hanleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={hanleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
