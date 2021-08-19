025835e1a7328bbd6ff48f907b30535c5e80f9bb
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-20 00:32:44 +0900

    12-3 Complete

--- 12-Sidebar-Modal/src/Sidebar.js
+++ 12-Sidebar-Modal/src/Sidebar.js
@@ -2,13 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import { FaTimes } from 'react-icons/fa';
 import { social, links } from './data';
+import { useGlobalContext } from './context';
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
   return (
-    <aside className={`sidebar`}>
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
       <div className='sidebar-header'>
         <img src={logo} className='logo' alt='coding addict' />
-        <button className='close-btn'>
+        <button className='close-btn' onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
@@ -21,6 +24,16 @@ const Sidebar = () => {
                 {icon}
                 {text}
               </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
             </li>
           );
         })}
