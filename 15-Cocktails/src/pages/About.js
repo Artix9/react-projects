128074a20e4bfb18b17d18bf66c43c8457c4b0cd
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-22 15:51:01 +0900

    15-3 About and Error

--- 15-Cocktails/src/pages/About.js
+++ 15-Cocktails/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum iste atque
+        mollitia. Sed illo voluptatibus ex corporis fugit quod? Earum totam
+        tenetur nesciunt hic illum sint in quibusdam sequi aspernatur, odio,
+        iure iusto illo blanditiis consequuntur, error expedita et!
+        Necessitatibus.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
