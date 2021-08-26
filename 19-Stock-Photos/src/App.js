9662f3315692b5b87b871634c7858e5302962f5f
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-26 21:19:45 +0900

    19-3 Scroll event

--- 19-Stock-Photos/src/App.js
+++ 19-Stock-Photos/src/App.js
@@ -30,6 +30,22 @@ function App() {
     fetchImages();
   }, []);
 
+  useEffect(() => {
+    const event = window.addEventListener('scroll', () => {
+      // console.log(`innerHeight ${window.innerHeight}`);
+      // console.log(`scrollY ${window.scrollY}`);
+      // console.log(`body height ${document.body.scrollHeight}`);
+
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        console.log('it wored');
+      }
+    });
+    return () => window.removeEventListener('scroll', event);
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('hello');
